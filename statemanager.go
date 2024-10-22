4eec19ed3e75bdd47d21d6ce2b97d6cc0745d977
Alessandro Bessi <abessi@nextbit.it>
2024-10-22 10:03:19 +0200

    generics support

--- statemanager.go
+++ statemanager.go
@@ -6,20 +6,20 @@ import (
 	"os"
 )
 
-// StateManager structure to encapsulate state management
-type StateManager struct {
+// StateManager manages file-based state for a generic type
+type StateManager[T any] struct {
 	FilePath string
-	State    interface{}
+	State    T
 }
 
 // NewStateManager creates a new StateManager instance
-func NewStateManager(filePath string, initialState interface{}) *StateManager {
-	sm := &StateManager{
+func NewStateManager[T any](filePath string, initialState T) *StateManager[T] {
+	sm := &StateManager[T]{
 		FilePath: filePath,
 		State:    initialState,
 	}
 
-	// Try loading the state from file
+	// Try loading the state from the file
 	err := sm.LoadState()
 	if err != nil {
 		fmt.Println("Starting with initial state:", initialState)
@@ -29,17 +29,17 @@ func NewStateManager(filePath string, initialState interface{}) *StateManager {
 }
 
 // LoadState reads the state from a file and updates the State field
-func (sm *StateManager) LoadState() error {
+func (sm *StateManager[T]) LoadState() error {
 	file, err := os.ReadFile(sm.FilePath)
 	if err != nil {
 		if os.IsNotExist(err) {
 			return nil // File doesn't exist, start with initial state
 		}
-		return err // Other errors (e.g., permission issues)
+		return err
 	}
 
 	// Unmarshal the JSON into the State
-	err = json.Unmarshal(file, sm.State)
+	err = json.Unmarshal(file, &sm.State)
 	if err != nil {
 		return fmt.Errorf("error parsing state file: %w", err)
 	}
@@ -48,7 +48,7 @@ func (sm *StateManager) LoadState() error {
 }
 
 // SaveState writes the current state to the file
-func (sm *StateManager) SaveState() error {
+func (sm *StateManager[T]) SaveState() error {
 	data, err := json.MarshalIndent(sm.State, "", "  ")
 	if err != nil {
 		return fmt.Errorf("error marshaling state: %w", err)
